0429f225013a34ad6b35041cdc0139af81f57e45
MatiasMaravi <matias.maravi@utec.edu.pe>
2023-10-09 00:55:58 -0500

    Terminado de optmizar la clase InvertIndex.py

--- src/classes/InvertIndex.py
+++ src/classes/InvertIndex.py
@@ -20,6 +20,7 @@ class InvertIndex:
                 self.length = data['length']
         except FileNotFoundError:
             print("El archivo de índice no existe. Debe construirlo primero usando la función `building`.") 
+
     def building(self, collection_text):
         # Procesamiento en paralelo
         def process_file(file_name):
@@ -48,36 +49,45 @@ class InvertIndex:
         }
         with open(self.index_file, 'w') as f:
             json.dump(data, f)
-            
+    
+    def validate_query(self, query_term_unic):
+        aux = set()
+        for term in query_term_unic:
+        # Validamos si existe el término en nuestros diccionarios
+            if term in self.idf:
+                aux.add(term)
+        return aux
+    
     def retrieval(self, query, k):
         self.load_index(self.index_file)
-        # diccionario para el score
-        score = {}
         # preprocesar la query: extraer los terminos unicos
         queryPrep = preprocesamiento(query)
+        query_term_unic=set(queryPrep)
+
+        # diccionario para el score
+        score = {}
         # aplicar similitud de coseno y guardarlo en el diccionario score
         for key in self.length.keys():
             score[key] = 0
-
-        query_term_unic=set(queryPrep)
+        #validamos si existe el termino en nuestros diccionarios
+        query_term_unic=self.validate_query(query_term_unic)
+        if(len(query_term_unic)==0):
+            return score
 
         lenght_query=[]
 
         for term in query_term_unic:
-            # Validamos si existe el término en nuestros diccionarios
-            if term not in self.idf and term not in self.index:
-                continue
-
             # calcular el tf-idf del query
             term_tf = math.log10(1+queryPrep.count(term))
-
             term_idf = self.idf[term]
             term_doc = self.index[term]
 
             lenght_query.append(term_tf*term_idf)
 
             for doc in term_doc:
-                score[doc] += self.index[term][doc]*term_idf*term_tf*term_idf
+                w_td = self.index[term][doc]*term_idf
+                w_tq = term_tf*term_idf
+                score[doc] += w_td * w_tq
 
         norma_query=np.linalg.norm(np.array(lenght_query))
 
