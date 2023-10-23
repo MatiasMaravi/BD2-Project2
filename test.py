152e6f25e6c09641d1dd926dc27b6a0409ede269
Jerimy2021 <jerimy.sandoval@utec.edu.pe>
2023-10-23 01:03:56 -0500

    Avances del proyecto

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import psutil
 import sys
 import json
 import nltk
+import os
+import numpy as np
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import RegexpTokenizer
 nltk.download('punkt')
@@ -14,32 +16,26 @@ collection = [
     "doc/doc5.txt"
 ]
 
-def BSBIndexConstruction (collection: list):
-    files = list()
-    mergedFile = "mergedFile.json"
 
-    while not allDocumentsProcessed(collection):
-        document = getCurrentDoc(collection)
-        tokenStream = parseDoc(document)
-        nFile = SPIMI_Invert(tokenStream)
-        files.append(nFile)
-
-    return mergeBlocks(files, mergedFile)
-
-def allDocumentsProcessed (collection: list):
+def allDocumentsProcessed(collection: list):
     # Retorna True si la lista está vacía
     return not collection
+
 
 def getCurrentDoc (collection: list):
     # Retorna el último valor de la lista
     return collection.pop()
 
+
 def parseDoc (document: str):
     with open("stoplist.txt", encoding='latin1') as file:
         stoplist = [line.rstrip().lower() for line in file]
     
+    doc_id = os.path.basename(document).split('.')[0]
+    
     # Obtener el texto del documento
-    texto = document.read()
+    with open(document, 'r', encoding='utf-8') as document:
+        texto = document.read()
     
     # Tokenizar
     tokens = []
@@ -53,8 +49,11 @@ def parseDoc (document: str):
     snowStemmer = SnowballStemmer(language='spanish')
     for i in range (0, len(tokens)):
         tokens[i] = snowStemmer.stem(tokens[i])
+    #Devolvemos una lista de tuplas (termino,doc_id) como dice la linea 4     
+    term_doc_tuples = [(term, doc_id) for term in tokens]
     
-    return tokens
+    return term_doc_tuples
+
 
 def SPIMI_Invert (tokenStream: list):
     outputFile = "outputFile.json"
@@ -62,28 +61,31 @@ def SPIMI_Invert (tokenStream: list):
 
     initialRam = psutil.virtual_memory().available
     usedRam = -1
-
+    print(f"{tokenStream} y tamaño {len(tokenStream)}")
+    
     while usedRam != 0 and tokenStream:
         currentRam = psutil.virtual_memory().available
         usedRam = initialRam - currentRam
         
         token = tokenStream.pop()
+        postingsList = np.full(10,-1)
         
         if token not in dictionary:
-            postingsList = addToDict(dictionary, token)
+            postingsList = addToDict(dictionary, token[0])
         else:
-            postingsList = getPostingsList(dictionary, token)
+            postingsList = getPostingsList(dictionary, token[0])
         
         if full(postingsList):
-            postingsList = doublePostingsList(dictionary, token)
+           postingsList = doublePostingsList(dictionary, token)
         
         addToPostingsList(postingsList, docID(token))
+        
+    sT = dict(sorted(dictionary.items()))
+    print(f"Lista ordenada \n{sT} y tamaño {len(sT)}")
     
-    sT = sortedTerms(dictionary)
     writeBlockFile(sT, dictionary, outputFile)
-
+    
     return outputFile
-
 
 def getPostingsList(dictionary: dict, token: str):
     return dictionary[token]
@@ -97,8 +99,13 @@ def addToPostingsList(postingsList: list, docID: int):
         postingsList.append(docID)
     return postingsList
 
-def doublePostingsList(dictionary: dict, token: str):
+def docID(token : str):
+    return int(token[1][-1])
 
+def doublePostingsList(dictionary: dict, token: str):
+    postingsList = getPostingsList(dictionary, token[0])
+    postingsList = np.concatenate((postingsList, np.full(10,-1)))
+    return postingsList
 
 def full(postingsList: list):
     #Para saber si usamos solo 20 mb cambiar a la memoria que vamos usar
@@ -122,4 +129,25 @@ def memoria_usada(tamanhio,n):
     else:
         return False
 
-print(memoria_usada(tamanhio_array_bytes([str(i) for i in range(0, 1000000)]),20))
+def writeBlockFile(sT: dict, dictionary: dict, outputFile: str):
+    with open(outputFile, 'w', encoding='utf-8') as file:
+        json.dump(sT, file, ensure_ascii=False, indent=4)
+
+def mergeBlocks(files: list, mergedFile: str):
+    with open(mergedFile, 'w', encoding='utf-8') as file:
+        for f in files:
+            with open(f, 'r', encoding='utf-8') as block:
+                data = json.load(block)
+                json.dump(data, file, ensure_ascii=False, indent=4)
+    return mergedFile
+def BSBIndexConstruction (collection: list):
+    files = list()
+    mergedFile = "mergedFile.json"
+    while not allDocumentsProcessed(collection):
+        document = getCurrentDoc(collection)
+        tokenStream = parseDoc(document)
+        nFile = SPIMI_Invert(tokenStream)
+        files.append(nFile)
+
+    return mergeBlocks(files, mergedFile)
+print(BSBIndexConstruction(collection))
