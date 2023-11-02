101bad6833d580130990249fd53e1e02b21d7526
MatiasMaravi <matias.maravi@utec.edu.pe>
2023-11-01 23:12:04 -0500

    terminado merge

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import os
+import json
 from SPIMI import BSBI
 
 # Construimos el indice
 
 Indice=BSBI(size_block=20480,archivo="spotify_songs.csv")
 Indice.SPIMI()
-flist = os.listdir("blocks")
-Indice.merged_blocks(flist)
+merged_index = Indice.init_merge()
+Indice.delete_blocks()
+# Guarda el índice invertido fusionado en un archivo JSON
+with open("index.json", "w") as f:
+    json.dump(merged_index, f, indent=4, ensure_ascii=False)
+
+print("Índice invertido fusionado guardado en index.json")
+Indice.equilibrar_blocks("index.json")
