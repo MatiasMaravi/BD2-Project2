ac981383dc59e115099201c6e1ca00a57a582dca
MatiasMaravi <matias.maravi@utec.edu.pe>
2023-11-25 14:06:29 -0500

    listo

--- src/utils/preprocesamiento.py
+++ src/utils/preprocesamiento.py
@@ -1,5 +1,6 @@
 import nltk
-
+# nltk.download('stopwords') Necesario al ejecutar por primera vez
+# nltk.download('punkt') Necesario al ejecutar por primera vez
 from nltk.stem.snowball import SnowballStemmer
 
 
@@ -12,6 +13,7 @@ def preprocesamiento(texto,idioma) -> list:
         return [stemmer.stem(word.lower()) for word in nltk.word_tokenize(texto) if word.isalpha() and word.lower() not in stoplist]
     elif idioma == 'en':
         #usamos la libreria nltk para descargar el stoplist en ingles
+        
         from nltk.corpus import stopwords
         stoplist = stopwords.words('english')
         stemmer = SnowballStemmer("english")
